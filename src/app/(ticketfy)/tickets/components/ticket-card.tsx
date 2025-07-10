592e3de4282faf47624570e0f87050def54f9d87
Evandro Calado <evandrocalado07@gmail.com>
2025-07-10 20:57:09 -0300

    refactor(tickets): Improve ticket card layout and content display

--- src/app/(ticketfy)/tickets/components/ticket-card.tsx
+++ src/app/(ticketfy)/tickets/components/ticket-card.tsx
@@ -13,28 +13,28 @@ type TicketCardProps = {
 
 export const TicketCard = ({ ticket }: TicketCardProps) => {
   return (
-    <div>
-      <Card>
-        <CardHeader>
-          <CardTitle className='flex items-center gap-2'>
-            {TICKET_ICONS[ticket.status]}
+    <Card className='p-4'>
+      <CardHeader className='px-0'>
+        <CardTitle className='flex items-center gap-2'>
+          {TICKET_ICONS[ticket.status]}
 
-            <span className='text-xl'>{ticket.title}</span>
-          </CardTitle>
-        </CardHeader>
+          <span className='text-xl'>{ticket.title}</span>
+        </CardTitle>
+      </CardHeader>
 
-        <CardContent>
-          <span className='text-muted-foreground'>{ticket.content}</span>
-        </CardContent>
+      <CardContent className='grow px-0'>
+        <span className='text-muted-foreground line-clamp-3'>
+          {ticket.content}
+        </span>
+      </CardContent>
 
-        <div className='border-border mx-5 flex items-center justify-between border-t p-0 pt-3'>
-          {TICKET_STATUS[ticket.status]}
+      <div className='border-border flex items-center justify-between border-t px-0 pt-3'>
+        {TICKET_STATUS[ticket.status]}
 
-          <Button asChild size='sm'>
-            <Link href={`/ticket/${ticket.id}`}>Ver mais</Link>
-          </Button>
-        </div>
-      </Card>
-    </div>
+        <Button asChild size='sm'>
+          <Link href={`/ticket/${ticket.id}`}>Ver mais</Link>
+        </Button>
+      </div>
+    </Card>
   );
 };
