7ad0bfc00428c8a90103ea320cbaa90d83437e0c
Evandro Calado <evandrocalado07@gmail.com>
2025-07-11 21:13:52 -0300

    refactor(footer): Improve footer links and styling

--- src/components/shared/footer.tsx
+++ src/components/shared/footer.tsx
@@ -2,41 +2,41 @@ import Link from 'next/link';
 
 import { GithubIcon, LinkedinIcon } from 'lucide-react';
 
-import { Button } from '../ui/button';
-
 export const Footer = () => {
   return (
     <footer>
-      <div className='container mx-auto flex flex-col items-center justify-between gap-3 p-5 md:flex-row'>
-        <span className='text-muted-foreground text-sm'>
+      <div className='text-muted-foreground container mx-auto flex flex-col items-center justify-between gap-3 p-5 md:flex-row'>
+        <span className='text-sm'>
           © {new Date().getFullYear()} Evandro Calado. Todos os direitos
           reservados.
         </span>
 
-        <span className='flex items-center gap-2'>
-          <Button asChild variant='ghost' size='icon'>
-            <Link
-              href='https://github.com/evandrocalado'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <GithubIcon />
-            </Link>
-          </Button>
-          <Button asChild variant='ghost' size='icon'>
-            <Link
-              href='https://www.linkedin.com/in/evandro-calado/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <LinkedinIcon />
-            </Link>
-          </Button>
+        <span className='flex items-center gap-4'>
+          <Link
+            href='https://github.com/evandrocalado'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:text-foreground duration-150'
+          >
+            <GithubIcon className='size-5' />
+          </Link>
+          <Link
+            href='https://www.linkedin.com/in/evandro-calado/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='hover:text-foreground duration-150'
+          >
+            <LinkedinIcon className='size-5' />
+          </Link>
         </span>
 
-        <span className='text-muted-foreground flex items-center gap-2 text-xs'>
-          <Link href='/'>Política de Privacidade</Link>
-          <Link href='/'>Termos de Uso</Link>
+        <span className='text-muted-foreground flex items-center gap-4 text-xs'>
+          <Link href='/' className='hover:text-foreground duration-150'>
+            Política de Privacidade
+          </Link>
+          <Link href='/' className='hover:text-foreground duration-150'>
+            Termos de Uso
+          </Link>
         </span>
       </div>
     </footer>
