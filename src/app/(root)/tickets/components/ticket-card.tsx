060c04c4789a47e79eaf6dce3e5f60be9c946568
Evandro Calado <evandrocalado07@gmail.com>
2025-07-19 17:48:38 -0300

    feat(tickets): link ticket card to detail page and improve styling

--- src/app/(root)/tickets/components/ticket-card.tsx
+++ src/app/(root)/tickets/components/ticket-card.tsx
@@ -1,8 +1,5 @@
 import Link from 'next/link';
 
-import { FileSymlinkIcon } from 'lucide-react';
-
-import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { formatCurrency } from '@/utils/format-currency';
 import { formatDate } from '@/utils/format-date';
@@ -17,37 +14,36 @@ type TicketCardProps = {
 
 export const TicketCard = ({ ticket }: TicketCardProps) => {
   return (
-    <Card className='p-4'>
-      <CardHeader className='px-0'>
-        <CardTitle className='flex items-center gap-2'>
-          {TICKET_ICONS[ticket.status]}
+    <Link
+      href={`/ticket/${ticket.id}`}
+      className='duration-150 ease-in-out hover:-translate-y-2'
+    >
+      <Card className='p-4'>
+        <CardHeader className='px-0'>
+          <CardTitle className='flex items-center gap-2'>
+            {TICKET_ICONS[ticket.status]}
 
-          <span className='text-xl'>{ticket.title}</span>
-        </CardTitle>
-      </CardHeader>
+            <span className='text-xl'>{ticket.title}</span>
+          </CardTitle>
+        </CardHeader>
 
-      <CardContent className='grow px-0'>
-        <div className='flex h-full flex-col justify-between'>
+        <CardContent className='grow px-0'>
           <span className='text-muted-foreground line-clamp-2'>
             {ticket.content}
           </span>
+        </CardContent>
 
-          <div className='text-muted-foreground/40 mt-4 flex items-center justify-between text-sm font-semibold'>
-            <span>{formatDate(ticket.deadline)}</span>
-            <span>{formatCurrency(ticket.bounty)}</span>
+        <div className='border-border flex items-center justify-between border-t px-0 pt-3'>
+          {TICKET_STATUS[ticket.status]}
+
+          <div className='flex items-center gap-2'>
+            <span className='text-xs'>{formatDate(ticket.deadline)}</span>
+            <span className='text-primary text-lg font-semibold'>
+              {formatCurrency(ticket.bounty)}
+            </span>
           </div>
         </div>
-      </CardContent>
-
-      <div className='border-border flex items-center justify-between border-t px-0 pt-3'>
-        {TICKET_STATUS[ticket.status]}
-
-        <Button asChild size='icon' aria-label='Ver ticket' title='Ver ticket'>
-          <Link href={`/ticket/${ticket.id}`}>
-            <FileSymlinkIcon />
-          </Link>
-        </Button>
-      </div>
-    </Card>
+      </Card>
+    </Link>
   );
 };
