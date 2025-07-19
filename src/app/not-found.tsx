6c18cf19b94008250e51b04d0121b3017ec20eef
Evandro Calado <evandrocalado07@gmail.com>
2025-07-18 21:37:36 -0300

    feat: add custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,31 @@
+import { Metadata } from 'next';
+import Link from 'next/link';
+
+import { HomeIcon } from 'lucide-react';
+
+import { EmptyError } from '@/components/shared/empty-error';
+import { Button } from '@/components/ui/button';
+import { homePath } from '@/utils/paths';
+
+export const metadata: Metadata = {
+  title: 'Ticket não encontrado',
+};
+
+const NotFound = () => {
+  return (
+    <div className='container mx-auto flex min-h-screen max-w-2xl flex-1 flex-col items-center justify-center gap-5 p-5'>
+      <EmptyError
+        label='Página não encontrada!'
+        button={
+          <Button asChild>
+            <Link href={homePath()}>
+              <HomeIcon /> Voltar para Home
+            </Link>
+          </Button>
+        }
+      />
+    </div>
+  );
+};
+
+export default NotFound;
