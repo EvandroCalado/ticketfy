818526fd216ffaaf20b88fe1071f4456237c25e9
Evandro Calado <evandrocalado07@gmail.com>
2025-07-19 21:50:23 -0300

    chore: add sign-up, sign-in, and forgot-password paths

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -4,3 +4,7 @@ export const ticketsPath = () => '/tickets';
 export const ticketPath = (ticketId: string) => `/ticket/${ticketId}`;
 export const ticketEditPath = (ticketId: string) => `/ticket/edit/${ticketId}`;
 export const ticketCreatePath = () => '/ticket/create';
+
+export const signUpPath = () => '/sign-up';
+export const signInPath = () => '/sign-in';
+export const forgotPasswordPath = () => '/forgot-password';
