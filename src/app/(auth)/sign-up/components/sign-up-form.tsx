ab8a84df7f457421b04898452622c9dedc8e72bf
Evandro Calado <evandrocalado07@gmail.com>
2025-07-20 17:27:16 -0300

    fix: rename component name

---
+++ src/app/(auth)/sign-up/components/sign-up-form.tsx
@@ -0,0 +1,98 @@
+'use client';
+
+import { useActionState } from 'react';
+
+import { INITIAL_STATE } from '@/app/(root)/ticket/create/constants/initial-create-state';
+import { Form } from '@/components/shared/form';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { formatErrorMessage } from '@/utils/format-error-message';
+
+import { signUp } from '../actions/sign-up';
+
+export const SignUpForm = () => {
+  const [state, dispatch, isPending] = useActionState(signUp, INITIAL_STATE);
+
+  const { fieldErrors, payload } = state;
+
+  return (
+    <Form state={state} action={dispatch}>
+      <div className='relative'>
+        <Label htmlFor='username' className='mb-1'>
+          Usuário
+        </Label>
+        <Input
+          id='username'
+          name='username'
+          type='text'
+          defaultValue={(payload?.get('username') as string) || ''}
+        />
+
+        {fieldErrors?.username && (
+          <p className='text-destructive absolute -bottom-5 text-xs'>
+            {formatErrorMessage(fieldErrors.username)}
+          </p>
+        )}
+      </div>
+
+      <div className='relative'>
+        <Label htmlFor='email' className='mb-1'>
+          Email
+        </Label>
+        <Input
+          id='email'
+          name='email'
+          type='email'
+          defaultValue={(payload?.get('email') as string) || ''}
+        />
+
+        {fieldErrors?.email && (
+          <p className='text-destructive absolute -bottom-5 text-xs'>
+            {formatErrorMessage(fieldErrors.email)}
+          </p>
+        )}
+      </div>
+
+      <div className='relative'>
+        <Label htmlFor='password' className='mb-1'>
+          Senha
+        </Label>
+        <Input
+          id='password'
+          name='password'
+          type='password'
+          defaultValue={(payload?.get('password') as string) || ''}
+        />
+
+        {fieldErrors?.password && (
+          <p className='text-destructive absolute -bottom-5 text-xs'>
+            {formatErrorMessage(fieldErrors.password)}
+          </p>
+        )}
+      </div>
+
+      <div className='relative'>
+        <Label htmlFor='confirmPassword' className='mb-1'>
+          Confirmar Senha
+        </Label>
+        <Input
+          id='confirmPassword'
+          name='confirmPassword'
+          type='password'
+          defaultValue={(payload?.get('confirmPassword') as string) || ''}
+        />
+
+        {fieldErrors?.confirmPassword && (
+          <p className='text-destructive absolute -bottom-5 text-xs'>
+            {formatErrorMessage(fieldErrors.confirmPassword)}
+          </p>
+        )}
+      </div>
+
+      <Button type='submit' className='mt-4 w-full' disabled={isPending}>
+        {isPending ? 'Cadastrando...' : 'Cadastrar'}
+      </Button>
+    </Form>
+  );
+};
