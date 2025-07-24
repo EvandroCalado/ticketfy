273750e75e70f2bb9ad32f88075e60fd50ef76f9
Evandro Calado <evandrocalado07@gmail.com>
2025-07-24 18:28:18 -0300

    feat: enhance sidebar with user avatar and improved styling

--- src/components/shared/sidebar.tsx
+++ src/components/shared/sidebar.tsx
@@ -9,8 +9,10 @@ import { User } from '@/generated/prisma';
 import { cn } from '@/lib/utils';
 import { ticketsPath } from '@/utils/paths';
 
+import { Avatar, AvatarFallback } from '../ui/avatar';
 import { Button } from '../ui/button';
 import { Card } from '../ui/card';
+import { Separator } from '../ui/separator';
 
 type SidebarProps = {
   user: User;
@@ -19,15 +21,35 @@ type SidebarProps = {
 export const Sidebar = ({ user }: SidebarProps) => {
   const pathname = usePathname();
 
-  console.log(user);
-
   return (
-    <Card className='animate-sidebar-from-left group h-full w-[64px] py-0 transition-all duration-200 hover:w-52'>
+    <Card className='animate-sidebar-from-left group h-full w-[64px] gap-2 py-0 transition-all duration-200 hover:w-52'>
+      <div className='flex items-center gap-2 p-3'>
+        <Avatar className='size-[38px]'>
+          <AvatarFallback className='bg-muted uppercase'>
+            {user.name[0]}
+          </AvatarFallback>
+        </Avatar>
+        <div className='pointer-events-none text-left text-xs font-semibold opacity-0 transition-opacity duration-200 group-hover:opacity-100'>
+          <span className='block capitalize'>{user.name}</span>
+          <span className='text-muted-foreground/40 block text-xs'>
+            {user.email}
+          </span>
+        </div>
+      </div>
+
+      <Separator />
+
       <div className='space-y-2 p-3'>
         <Button
-          className={cn('hover:bg-muted w-full justify-start bg-transparent', {
-            'bg-primary hover:bg-primary/80': pathname === ticketsPath(),
-          })}
+          aria-label='Todos os tickets'
+          title='Todos os tickets'
+          className={cn(
+            'hover:bg-muted text-foreground w-full justify-start bg-transparent',
+            {
+              'bg-primary hover:bg-primary/80 text-primary-foreground':
+                pathname === ticketsPath(),
+            },
+          )}
           asChild
         >
           <Link href={ticketsPath()}>
@@ -39,10 +61,15 @@ export const Sidebar = ({ user }: SidebarProps) => {
         </Button>
 
         <Button
-          className={cn('hover:bg-muted w-full justify-start bg-transparent', {
-            'bg-primary hover:bg-primary/80':
-              pathname === `/tickets/user/${user.id}`,
-          })}
+          aria-label='Meus tickets'
+          title='Meus tickets'
+          className={cn(
+            'hover:bg-muted text-foreground w-full justify-start bg-transparent',
+            {
+              'bg-primary hover:bg-primary/80 text-primary-foreground':
+                pathname === `/tickets/user/${user.id}`,
+            },
+          )}
           asChild
         >
           <Link href={`/tickets/user/${user.id}`}>
