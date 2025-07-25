d516ed7d3e7fa3668d8c0ddd14eb82ebf0076019
Evandro Calado <evandrocalado07@gmail.com>
2025-07-25 19:31:36 -0300

    refactor(header): simplify header and navigation links

--- src/components/shared/header-links.tsx
+++ src/components/shared/header-links.tsx
@@ -8,12 +8,12 @@ export const HeaderLinks = () => {
   const pathname = usePathname();
 
   return (
-    <nav className='hidden items-center gap-2 md:flex md:gap-5'>
+    <nav className='flex items-center gap-3 md:gap-5'>
       {navLinks.map(link => (
         <Link
           key={link.href}
           href={link.href}
-          className={cn('hover:text-primary w-16 duration-150', {
+          className={cn('hover:text-primary duration-150', {
             'text-primary': pathname === link.href,
           })}
         >
--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -2,16 +2,14 @@
 
 import Link from 'next/link';
 
-import { signOut } from '@/actions/sign-out';
 import { useAuth } from '@/hooks/use-auth';
-import { signInPath, signUpPath } from '@/utils/paths';
+import { signInPath } from '@/utils/paths';
 
 import { Button } from '../ui/button';
 import { Card } from '../ui/card';
 import { DarkMode } from './dark-mode';
 import { HeaderLinks } from './header-links';
 import { Logo } from './logo';
-import { SubmitButton } from './submit-button';
 
 export const Header = () => {
   const [user, isFetchUser] = useAuth();
@@ -22,26 +20,15 @@ export const Header = () => {
     <header className='border-border animate-header-from-top bg-background absolute top-0 right-0 left-0 z-50 m-5'>
       <Card className='mx-auto flex max-w-3xl flex-row items-center justify-between p-3'>
         <Logo />
-
         <HeaderLinks />
 
         <div className='flex items-center gap-2 md:gap-3'>
           <DarkMode />
 
-          {user ? (
-            <form action={signOut}>
-              <SubmitButton className='w-24'>Sair</SubmitButton>
-            </form>
-          ) : (
-            <>
-              <Button variant='ghost' className='w-24' asChild>
-                <Link href={signUpPath()}>Cadastrar</Link>
-              </Button>
-
-              <Button className='w-24' asChild>
-                <Link href={signInPath()}>Entrar</Link>
-              </Button>
-            </>
+          {!user && (
+            <Button asChild>
+              <Link href={signInPath()}>Entrar</Link>
+            </Button>
           )}
         </div>
       </Card>
