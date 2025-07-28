d0c2df39c4058e064b3a9d1ae7c47b4aeb716188
Evandro Calado <evandrocalado07@gmail.com>
2025-07-28 16:07:54 -0300

    feat(account): add account info form with username, email and password fields

--- src/app/(root)/(protected)/account/components/account-info.tsx
+++ src/app/(root)/(protected)/account/components/account-info.tsx
@@ -1,3 +1,6 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Separator } from '@/components/ui/separator';
 
 export const AccountInfo = () => {
@@ -5,6 +8,33 @@ export const AccountInfo = () => {
     <div>
       <h2 className='text-xl font-semibold'>Informações</h2>
       <Separator className='my-4' />
+
+      <form className='space-y-4'>
+        <div>
+          <Label htmlFor='username' className='mb-1'>
+            Usuário
+          </Label>
+          <Input id='username' name='username' />
+        </div>
+
+        <div>
+          <Label htmlFor='email' className='mb-1'>
+            Email
+          </Label>
+          <Input id='email' name='email' type='email' disabled />
+        </div>
+
+        <div>
+          <Label htmlFor='password' className='mb-1'>
+            Senha
+          </Label>
+          <Input id='password' name='password' type='password' />
+        </div>
+
+        <Button type='submit' className='mt-4'>
+          Salvar
+        </Button>
+      </form>
     </div>
   );
 };
---
+++ src/utils/avatar-options.ts
@@ -0,0 +1,26 @@
+export const avatarOptions = [
+  {
+    bg: 'bg-gradient-to-br from-yellow-300 to-orange-300',
+    text: 'text-yellow-800',
+  },
+  {
+    bg: 'bg-gradient-to-br from-green-300 to-lime-300',
+    text: 'text-green-800',
+  },
+  {
+    bg: 'bg-gradient-to-br from-red-300 to-orange-300',
+    text: 'text-red-800',
+  },
+  {
+    bg: 'bg-gradient-to-br from-blue-300 to-cyan-300',
+    text: 'text-blue-800',
+  },
+  {
+    bg: 'bg-gradient-to-br from-purple-300 to-pink-300',
+    text: 'text-purple-800',
+  },
+  {
+    bg: 'bg-gradient-to-br from-pink-300 to-orange-300',
+    text: 'text-pink-800',
+  },
+];
