0a0975d8074a8c400f698d0efbebb98cac2ea7d6
Evandro Calado <evandrocalado07@gmail.com>
2025-08-05 09:11:26 -0300

    refactor: remove useActionState hook and use useTransition in comment form and delete button

--- src/app/(root)/(protected)/tickets/[ticketId]/components/comment-create-form.tsx
+++ src/app/(root)/(protected)/tickets/[ticketId]/components/comment-create-form.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useActionState } from 'react';
+import { useState, useTransition } from 'react';
 
-import { Form } from '@/components/shared/form';
+import { toast } from 'sonner';
+
 import { Button } from '@/components/ui/button';
 import { Textarea } from '@/components/ui/textarea';
 import { INITIAL_ACTION_STATE } from '@/constants/initial-create-state';
@@ -15,13 +16,43 @@ type CommentCreateFormProps = {
 };
 
 export const CommentCreateForm = ({ ticketId }: CommentCreateFormProps) => {
-  const [state, dispatch, isPending] = useActionState(
-    createComment.bind(null, ticketId),
-    INITIAL_ACTION_STATE,
+  const [fieldErrors, setFieldErrors] = useState(
+    INITIAL_ACTION_STATE.fieldErrors,
   );
+  const [isPending, startTransition] = useTransition();
+
+  const handleCreateComment = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+
+    startTransition(async () => {
+      const result = await createComment(
+        ticketId,
+        INITIAL_ACTION_STATE,
+        formData,
+      );
+      if (result.status === 'success' && result.message) {
+        toast.success(result.message);
+        form.reset();
+      }
+
+      if (result.status === 'error' && result.fieldErrors) {
+        setFieldErrors(result.fieldErrors);
+      }
+
+      if (result.status === 'error' && result.message) {
+        toast.error(result.message);
+      }
+    });
+  };
 
   return (
-    <Form className='flex flex-col items-end' state={state} action={dispatch}>
+    <form
+      className='flex flex-col items-end gap-2'
+      onSubmit={handleCreateComment}
+    >
       <div className='relative w-full'>
         <Textarea
           name='content'
@@ -29,16 +60,16 @@ export const CommentCreateForm = ({ ticketId }: CommentCreateFormProps) => {
           disabled={isPending}
         />
 
-        {state.fieldErrors?.content && (
+        {fieldErrors?.content && (
           <p className='text-destructive absolute -bottom-5 text-xs'>
-            {formatErrorMessage(state.fieldErrors.content)}
+            {formatErrorMessage(fieldErrors.content)}
           </p>
         )}
       </div>
 
-      <Button type='submit' disabled={isPending}>
+      <Button type='submit' disabled={isPending} className='w-fit'>
         {isPending ? 'Enviando...' : 'Enviar'}
       </Button>
-    </Form>
+    </form>
   );
 };
--- src/app/(root)/(protected)/tickets/[ticketId]/components/comment-delete-button.tsx
+++ src/app/(root)/(protected)/tickets/[ticketId]/components/comment-delete-button.tsx
@@ -1,9 +1,12 @@
 'use client';
 
-import { TrashIcon } from 'lucide-react';
+import { useTransition } from 'react';
+
+import { Loader2Icon, TrashIcon } from 'lucide-react';
+import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/button';
-import { useConfirmDialog } from '@/hooks/use-confirm-dialog';
+import { INITIAL_ACTION_STATE } from '@/constants/initial-create-state';
 
 import { deleteComment } from '../actions/delete-comment';
 
@@ -14,19 +17,33 @@ type CommentDeleteButtonProps = {
 export const CommentDeleteButton = ({
   commentId,
 }: CommentDeleteButtonProps) => {
-  const [trigger, dialog] = useConfirmDialog({
-    action: deleteComment.bind(null, commentId),
-    trigger: (
-      <Button variant='destructive' size='icon'>
-        <TrashIcon />
-      </Button>
-    ),
-  });
+  const [isPending, startTransition] = useTransition();
+
+  const handleDeleteComment = () => {
+    startTransition(async () => {
+      const result = await deleteComment(
+        commentId,
+        INITIAL_ACTION_STATE,
+        new FormData(),
+      );
+      if (result.status === 'success' && result.message) {
+        toast.success(result.message);
+      }
+
+      if (result.status === 'error' && result.message) {
+        toast.error(result.message);
+      }
+    });
+  };
 
   return (
-    <>
-      {trigger}
-      {dialog}
-    </>
+    <Button
+      variant='destructive'
+      size='icon'
+      disabled={isPending}
+      onClick={handleDeleteComment}
+    >
+      {isPending ? <Loader2Icon className='animate-spin' /> : <TrashIcon />}
+    </Button>
   );
 };
