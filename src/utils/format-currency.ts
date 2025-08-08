e16b88662ce0cf46d065c737fd61010160f6687e
Evandro Calado <evandrocalado07@gmail.com>
2025-08-07 21:31:42 -0300

    fix(format-currency): update toCent and fromCent functions to return string format for consistency

--- src/utils/format-currency.ts
+++ src/utils/format-currency.ts
@@ -1,13 +1,13 @@
 import { MyBig } from '@/lib/big';
 
 export const toCent = (amount: string) =>
-  new MyBig(amount).mul(100).round(2).toNumber();
+  new MyBig(amount).mul(100).round(2).toString();
 
-export const fromCent = (amount: number) =>
-  new MyBig(amount).div(100).round(2).toNumber();
+export const fromCent = (amount: string) =>
+  new MyBig(amount).div(100).round(2).toString();
 
-export const formatCurrency = (amount: number) =>
+export const formatCurrency = (amount: string) =>
   Intl.NumberFormat('pt-BR', {
     style: 'currency',
     currency: 'BRL',
-  }).format(fromCent(amount));
+  }).format(Number(fromCent(amount)));
