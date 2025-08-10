8b40bb71d7069c9c6f9fc70e31040737403d5760
Evandro Calado <evandrocalado07@gmail.com>
2025-08-10 14:31:57 -0300

    refactor: Handle dates without timestamps in formatDate

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,9 +1,17 @@
 export const formatDate = (dateString: string) => {
-  const date = new Date(dateString);
+  let date: Date;
+
+  if (dateString.includes('T')) {
+    date = new Date(dateString);
+  } else {
+    const [year, month, day] = dateString.split('-').map(Number);
+    date = new Date(Date.UTC(year, month - 1, day));
+  }
 
   return date.toLocaleDateString('pt-BR', {
     day: '2-digit',
     month: 'long',
     year: 'numeric',
+    timeZone: 'UTC',
   });
 };
