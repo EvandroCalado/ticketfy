42734b58a12d93417aa5301547e93af20195e1d6
Evandro Calado <evandrocalado07@gmail.com>
2025-08-25 21:29:54 -0300

    feat: check user organization before switching

--- src/app/(root)/(protected)/organizations/actions/switch-organization.ts
+++ src/app/(root)/(protected)/organizations/actions/switch-organization.ts
@@ -4,10 +4,27 @@ import { requireAuthOnly } from '@/actions/require-auth';
 import { prisma } from '@/lib/prisma';
 import { formErrorHandler } from '@/utils/form-error-handler';
 
+import { getOrganizationByUser } from './get-organization-by-user';
+
 export const switchOrganization = async (organizationId: string) => {
   const { user } = await requireAuthOnly();
 
   try {
+    const organizations = await getOrganizationByUser();
+
+    const canSwitch = organizations.some(
+      organization => organization.id === organizationId,
+    );
+
+    if (!canSwitch) {
+      return {
+        success: false,
+        message: 'Organização não encontrada',
+        fieldErrors: undefined,
+        payload: undefined,
+      };
+    }
+
     await prisma.membership.updateMany({
       where: {
         userId: user.id,
